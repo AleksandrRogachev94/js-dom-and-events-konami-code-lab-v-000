ba39fd2f31fa422d382ae1d13d3888fe84eb3aa8
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-12-11 00:06:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,14 +2,14 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  let i = 0
+  let index = 0
   document.addEventListener('keydown', function(e){
     const key = parseInt(e.which || e.detail)
-    if(key === code[i]){
-      i++
-      if(code[i] === code.length){
+    if(key === code[index]){
+      index++
+      if(code[index] === code.length){
         alert("You did it!")
-        i = 0
+        index = 0
       }
     } else{
       i = 0
