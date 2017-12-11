de6f7a7f45e3052f8198dfbca83a072c5eb39c11
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-12-11 00:08:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ function init() {
     if(key === code[index]){
       index++
       if(index === code.length){
-        alert("You did it!")
+        alert("Hurray!")
         index = 0
       }
     } else{
